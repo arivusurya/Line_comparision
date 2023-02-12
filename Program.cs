7a75e9a3d399ab90e288e0b0af6d737ae9ad3c42
arivusurya <arivusurya.3@gmail.com>
2023-02-12 15:46:51 +0530

    line equality

--- Program.cs
+++ Program.cs
@@ -1,23 +1,46 @@
 ﻿using System;
-
+using Lineequal;
 namespace LineComparision{
 
     class Program{
 
         public static void Main(string[] args){
-            Console.WriteLine("enter the value of two coordinates : ");
-            Console.WriteLine("enter the value of X1 coordinate : ");
+           Console.WriteLine("Enter the values for the first line:");
+            Console.WriteLine("Enter X1: ");
             double x1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("enter the value of y1 coordinate : ");
-            double y1 =  Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("enter the value of X2 coordinate : ");
-            double x2 =  Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("enter the value of y2 coordinate : ");
-            double y2 =  Convert.ToDouble(Console.ReadLine());
-            Lengthofline.Linelegth(x1,y1,x2,y2);
+            Console.WriteLine("Enter Y1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            MyLine line1 = new MyLine(x1, y1, x2, y2);
+
+            Console.WriteLine("Enter the values for the second line:");
+            Console.WriteLine("Enter X1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+            MyLine line2 = new MyLine(x1, y1, x2, y2);
+            Program.compare(line1,line2);
+
+        
         }
+          public static void compare(MyLine line1,MyLine line2){
+            double m  = (line1.Y1-line1.Y2)/(line1.X1-line1.X2);
+            double m1  = (line2.Y1-line2.Y2)/(line2.X1-line2.X2);
+            if (m == m1){
+                Console.WriteLine("lines are parallel");
+            }else{
+                      Console.WriteLine("lines are not parallel");
+    }
     }
 
+}
 }
 
 
